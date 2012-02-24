ecb5891ff764c60973f00d5a30329419cc18f87e
Hans-Gunther Schmidt <hgs@cloudcontrol.de>
2012-02-24 11:05:17 +0100

    still fixing setup.py issues

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@ import os
 from setuptools import find_packages
 import version
 
-if not os.path.exists(os.path.join(os.environ['HOME'], '.ec2ssh')):
-    os.mkdir(os.path.join(os.environ['HOME'], '.ec2ssh'), 0755)
-
 required = ['boto']
 
 extra_options = dict(
@@ -34,7 +31,7 @@ setup(
     package_dir={'': 'src'},
     scripts=['ec2-to-ssh', 'src/ec2-instances.py'],
     data_files=[
-        ( os.path.join(os.environ['HOME'], '.ec2ssh'), ['conf/settings.cfg'] )
+        (os.path.join(os.environ['HOME'], '.ec2ssh'), [os.path.join('conf','settings.cfg')])
     ],
     download_url=DOWNLOAD_URL,
     license='Apache 2.0',
